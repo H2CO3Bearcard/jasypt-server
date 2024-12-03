094097aa430ae144927ee65a31c730b91616fc02
shuo.zhai <shuo.zhai@avatr.com>
2024-12-03 17:22:30 +0800

    hello

--- src/main/java/com/avatr/jasypt_server/controller/Hello.java
+++ src/main/java/com/avatr/jasypt_server/controller/Hello.java
@@ -1,13 +1,46 @@
 package com.avatr.jasypt_server.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 
 @RestController
 public class Hello {
-    @GetMapping("/hello")    
-    public String hello() {
-        return "hello";
+    @GetMapping(value = "/hello")
+    @Operation(summary = "hello", description = "hello", method = "GET", tags = "hello", responses = {
+            @ApiResponse(responseCode = "200", description = "Successful operation", content = {
+                @Content(mediaType = "application/json", schema = @Schema( implementation = HelloResponse.class))
+            }),
+            @ApiResponse(responseCode = "405", description = "Invalid input")
+    })
+    public HelloResponse hello(@RequestParam(required = false, defaultValue = "World") String name) {
+        HelloResponse helloResponse = new HelloResponse();
+        helloResponse.setMessage("Hello " + name);
+        return helloResponse;
+    }
+
+    @GetMapping(value = "/")
+    public String index() {
+        return new String("hello world");
+    }
+    
+}
+
+@lombok.Data
+class HelloResponse {
+    private String message;
+    public HelloResponse() {
     }
 }
+
+
+
